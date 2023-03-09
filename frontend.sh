3a22dd709b8f2d8f78ebb213a0d8cb60fdeca84c
chitra <chmanu.chinu@gmail.com>
2023-03-09 11:23:20 +0000

    update roboshop-shell

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,4 @@
-code_dir=$(pwd)
-log_file=/tmp/roboshop.log
-rm -rf ${log_file}
-
-print_head() {
-   echo -e "\e[35m$1\e[0m" 
-}
+source common.sh
 
 print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
