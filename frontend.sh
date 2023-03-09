7f771a557b6bfa42eebda564c5f5cc15ac7c0d62
chitra <chmanu.chinu@gmail.com>
2023-03-09 07:19:08 +0000

    update roboshop-shellll

--- frontend.sh
+++ frontend.sh
@@ -3,5 +3,6 @@ yum install nginx -y
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
+cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl enable nginx 
 systemctl start nginx
