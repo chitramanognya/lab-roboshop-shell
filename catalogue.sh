f7141d118ba699c1f791f53987a9df19bae422a9
chitra <chmanu.chinu@gmail.com>
2023-03-09 11:39:18 +0000

    update roboshop-shell

--- catalogue.sh
+++ catalogue.sh
@@ -1,21 +1,50 @@
 source common.sh
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app 
-rm -rf /app/*
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
-cd /app 
-unzip /tmp/catalogue.zip
-cd /app 
-npm install 
-cp configs/catalogue.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl enable catalogue 
-systemctl start catalogue
 
-cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
+print_head "Configure NodeJs Repo"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 
-mongo --host mongodb.learndevopsb71shop.site </app/schema/catalogue.js
+print_head "Install NodeJs"
+yum install nodejs -y &>>${log_file}
+
+print_head "Create Roboshop User"
+useradd roboshop &>>${log_file}
+
+print_head "Create Application Directory"
+mkdir /app  &>>${log_file}
+
+
+print_head "Delete Old Content"
+rm -rf /app/* &>>${log_file}
+
+print_head "Downloading App content"
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
+cd /app 
+
+print_head "Extracting App content"
+unzip /tmp/catalogue.zip &>>${log_file}
+
+
+print_head "Installing NodeJs Dependencies"
+npm install &>>${log_file}
+
+print_head "copy SystemD Service file"
+cp configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
+
+print_head "Reload SystemD"
+systemctl daemon-reload &>>${log_file}
+
+print_head "Enable Catalogue service"
+systemctl enable catalogue &>>${log_file} 
+
+print_head "Start Catalogue service"
+systemctl start catalogue &>>${log_file}
+
+print_head "Copy MongoDB Repo File"
+cp configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+
+print_head "Install Mongo Client"
+yum install mongodb-org-shell -y &>>${log_file}
+
+print_head "Load Schema"
+mongo --host mongodb.learndevopsb71shop.site </app/schema/catalogue.js &>>${log_file}
