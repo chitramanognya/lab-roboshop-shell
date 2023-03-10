b64c35fa86f6e1a0a8c01ef16f54ac35af7318f4
chitra <chmanu.chinu@gmail.com>
2023-03-10 12:27:19 +0000

    update roboshop-shell

--- mysql.sh
+++ mysql.sh
@@ -28,5 +28,8 @@ systemctl restart mysqld &>>${log_file}
 status_check $?
 
 print_head "Set Password"
+echo show databases | mysql -uroot -p${mysql_root_password} &>>${log_file}
+if [ $? -ne 0 ]; then
 mysql_secure_installation --set-root-pass ${mysql_root_password} &>>${log_file}
+fi
 status_check $?
