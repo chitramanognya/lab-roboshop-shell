aac5f9de9a2dff8f9602ba8b19b493fa23e9db7f
chitra <chmanu.chinu@gmail.com>
2023-03-10 13:43:36 +0000

    update roboshop-shell

--- shipping.sh
+++ shipping.sh
@@ -1,5 +1,12 @@
 source common.sh
 
+mysql_root_password=$1
+
+if [ "${mysql_root_password}" == "mysql" ]; then
+   echo "Missing MySQL Root Password arguement"
+   exit 1
+fi
+
 component=shipping
 schema_type="mysql"
 java
