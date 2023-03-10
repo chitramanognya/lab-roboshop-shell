1b2d67a6bdd82e7ed77aef1ed9ffe472117cc39c
chitra <chmanu.chinu@gmail.com>
2023-03-10 12:11:57 +0000

    update roboshop-shell

--- mysql.sh
+++ mysql.sh
@@ -7,22 +7,22 @@ exit 1
 fi
 
 
-print_head"Disabling MySQL 8 Version"
+print_head "Disabling MySQL 8 Version"
 dnf module disable mysql -y
 status_check $?
 
-print_head"Installing MySQL Server"
+print_head "Installing MySQL Server"
 yum install mysql-community-server -y
 status_check $?
 
-print_head"Enable MYSQL Service"
+print_head "Enable MYSQL Service"
 systemctl enable mysqld
 status_check $?
 
-print_head"Start MYSQL Service"
+print_head "Start MYSQL Service"
 systemctl restart mysqld  
 status_check $?
 
-print_head"Set Password"
+print_head "Set Password"
 mysql_secure_installation --set-root-pass ${mysql_root_password
 status_check $?
