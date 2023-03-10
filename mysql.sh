c5ab190e4d4f9937f7a0ace226c94ff427d004dd
chitra <chmanu.chinu@gmail.com>
2023-03-10 12:05:15 +0000

    update roboshop-shell

---
+++ mysql.sh
@@ -0,0 +1,28 @@
+source common.sh
+
+mysql_root_password=$1
+if[ -z "${1}" ]; then
+echo "Missing MYSQL Root Password argument"
+exit 1
+fi
+
+
+print_head"Disabling MySQL 8 Version"
+dnf module disable mysql -y
+status_check $?
+
+print_head"Installing MySQL Server"
+yum install mysql-community-server -y
+status_check $?
+
+print_head"Enable MYSQL Service"
+systemctl enable mysqld
+status_check $?
+
+print_head"Start MYSQL Service"
+systemctl restart mysqld  
+status_check $?
+
+print_head"Set Password"
+mysql_secure_installation --set-root-pass ${mysql_root_password
+status_check $?
